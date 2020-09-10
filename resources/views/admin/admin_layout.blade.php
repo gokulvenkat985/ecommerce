fca5c89bd8074c927e36391f109cbed1f9d4276d
gokul <gokulvenkat1010@gmail.com>
2020-09-10 23:45:44 +0530

    new brnach

--- resources/views/admin/admin_layout.blade.php
+++ resources/views/admin/admin_layout.blade.php
@@ -72,7 +72,7 @@
         <a href="#" class="sl-menu-link">
           <div class="sl-menu-item">
             <i class="menu-item-icon ion-ios-pie-outline tx-20"></i>
-            <span class="menu-item-label">Categories</span>
+            <span class="menu-item-label">matagories</span>
             <i class="menu-item-arrow fa fa-angle-down"></i>
           </div><!-- menu-item -->
         </a><!-- sl-meu-link -->
